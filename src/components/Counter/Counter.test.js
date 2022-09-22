62e119ca3deea4b3b0b8529d811c9bb4062ca9e5
kemalbekcan <kemalbekcan@gmail.com>
2022-09-22 12:32:36 +0300

    initial

---
+++ src/components/Counter/Counter.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Counter from "./index";
+
+describe("Counter Tests", () => {
+  let increaseBtn, decreaseBtn, count;
+  beforeEach(() => {
+    render(<Counter />);
+    count = screen.getByText("0");
+    increaseBtn = screen.getByText("Increase");
+    decreaseBtn = screen.getByText("Decrease");
+  });
+
+  test("increase btn", () => {
+    userEvent.click(increaseBtn);
+    expect(count).toHaveTextContent("1");
+  });
+
+  test("decrease btn", () => {
+    userEvent.click(decreaseBtn);
+    expect(count).toHaveTextContent("-1");
+  });
+});
